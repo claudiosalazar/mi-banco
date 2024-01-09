fede2257d486a3119d1f1c6472f7cbf0b1059423
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-08 23:24:41 -0300

    creacion de funcion logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import * as $ from 'jquery'; // Importar la biblioteca jQuery
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router) { }
 
+  showConfirmLogoutModal() {
+    // Muestra el modal de confirmación
+    ($('#confirmLogoutModal') as any).modal('show'); // Agregar el tipo de dato correcto para el objeto jQuery
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
