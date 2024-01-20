6e6a511592a4ee0ea8a960dfa1da3fcd3012650d
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-20 01:06:35 -0300

    Creacion de json model y services para datos de tabla transacciones

--- src/app/components/main/transacciones/transacciones.component.ts
+++ src/app/components/main/transacciones/transacciones.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HistorialTransaccionesService } from '../../../services/historial-transacciones.service';
+import { HistorialTransacciones } from '../../../../assets/models/historial-transacciones.model';
 
 @Component({
   selector: 'app-transacciones',
   templateUrl: './transacciones.component.html'
 })
-export class TransaccionesComponent {
 
+export class TransaccionesComponent implements OnInit {
+  data: HistorialTransacciones[] = [];
+  sortColumn = '';
+  sortAscending = true;
+
+  constructor(private historialTransaccionesService: HistorialTransaccionesService) { }
+
+  ngOnInit() {
+    this.historialTransaccionesService.getTransacciones().subscribe(transacciones => {
+      this.data = transacciones;
+    });
+  }
+
+  sort(column: string) {
+    if (this.sortColumn === column) {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      this.sortColumn = column;
+      this.sortAscending = true;
+    }
+
+    this.data.sort((a, b) => {
+      if (a[this.sortColumn as keyof HistorialTransacciones] < b[this.sortColumn as keyof HistorialTransacciones]) {
+        return this.sortAscending ? -1 : 1;
+      } else if (a[this.sortColumn as keyof HistorialTransacciones] > b[this.sortColumn as keyof HistorialTransacciones]) {
+        return this.sortAscending ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+  }
 }
---
+++ src/assets/models/historial-transacciones.model.ts
@@ -0,0 +1,7 @@
+export interface HistorialTransacciones {
+  col1: number;
+  col2: number;
+  col3: number;
+  col4: number;
+  col5: number;
+}
