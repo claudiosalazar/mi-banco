791395e2edbc55bddd79ee887aba7aa1497605bb
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-20 01:48:42 -0300

    Se carga el nombre del usuario en header llamando el dato desde service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
 import * as $ from 'jquery'; // Importar la biblioteca jQuery
+import { DatosUsuarioService } from 'src/app/services/datos-usuario.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private authService: AuthService, private router: Router) { }
+export class HeaderComponent implements OnInit {
+
+  nombreUserCompleto: string | undefined;
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private datosUsuarioService: DatosUsuarioService
+  ) { }
+
+  ngOnInit() {
+    this.datosUsuarioService.getDatosUsuario().subscribe(datos => {
+      this.nombreUserCompleto = datos.nombreUserCompleto;
+    });
+  }
 
   showConfirmLogoutModal() {
     // Muestra el modal de confirmación
