814e90c41ca09368385c2a7feffbb0406d9d6cbe
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-22 01:10:26 -0300

    Tabla transacciones con paginador

--- src/app/components/main/transacciones/transacciones.component.ts
+++ src/app/components/main/transacciones/transacciones.component.ts
@@ -1,19 +1,42 @@
 import { Component, NgIterable, OnInit } from '@angular/core';
 import { DatosUsuarioService } from '../../../services/datos-usuario.service';
-import { DatosUsuarioActual } from '../../../../assets/models/datos-usuario.model';
 
 @Component({
   selector: 'app-transacciones',
   templateUrl: './transacciones.component.html'
 })
 export class TransaccionesComponent implements OnInit {
-  transacciones: NgIterable<any> | null | undefined;
+  transacciones: any[] = [];
+  paginaActual = 1;
+  elementosPorPagina = 5;
 
   constructor(private datosUsuarioService: DatosUsuarioService) { }
 
   ngOnInit() {
     this.datosUsuarioService.getDatosUsuario().subscribe(datos => {
-      this.transacciones = datos.transacciones as unknown as NgIterable<any>;
+      this.transacciones = (datos.transacciones as unknown as Array<any>).slice().reverse();
     });
+  }
+
+  get transaccionesPaginadas() {
+    const inicio = (this.paginaActual - 1) * this.elementosPorPagina;
+    const fin = inicio + this.elementosPorPagina;
+    return this.transacciones.slice(inicio, fin);
+  }
+
+  siguientePagina() {
+    if (this.paginaActual < Math.ceil(this.transacciones.length / this.elementosPorPagina)) {
+      this.paginaActual++;
+    }
+  }
+
+  paginaAnterior() {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+    }
+  }
+
+  range(start: number, end: number) {
+    return Array.from({length: end - start}, (v, k) => k + start);
   }
 }
