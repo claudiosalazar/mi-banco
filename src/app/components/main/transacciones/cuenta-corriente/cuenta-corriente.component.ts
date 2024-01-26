2d6f2fa2de8c94b4685d866dc095bbbfa0eaf5b9
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-25 21:40:53 -0300

    Se estan cargando de manera correcta los datos de la tabla cuenta corriente

--- src/app/components/main/transacciones/cuenta-corriente/cuenta-corriente.component.ts
+++ src/app/components/main/transacciones/cuenta-corriente/cuenta-corriente.component.ts
@@ -18,11 +18,11 @@ export class CuentaCorrienteComponent implements OnInit {
     this.datosUsuarioService.getDatosUsuario().subscribe(datos => {
       this.datosUsuarioActual = datos;
       this.saldo = parseFloat(this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteSaldo);
-      this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.reverse();
       for (let trans of this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans) {
         this.saldo = this.saldo - trans.cargo + trans.abono;
         trans.saldoFinal = this.saldo;
       }
+      this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.reverse();
       console.log(this.datosUsuarioActual);
     }, error => {
       console.error('Error obteniendo los datos: ', error);
