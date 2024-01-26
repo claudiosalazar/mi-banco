7cb52a0443b2d3cba10a04a420d737a737e77ec3
Claudio Salazar <claudiosv@Mac-mini.local>
2024-01-25 22:55:54 -0300

    Se estan ordenando las columnas y la animacion del icono de th esta funcionando correctamente

--- src/app/components/main/transacciones/cuenta-corriente/cuenta-corriente.component.ts
+++ src/app/components/main/transacciones/cuenta-corriente/cuenta-corriente.component.ts
@@ -28,4 +28,69 @@ export class CuentaCorrienteComponent implements OnInit {
       console.error('Error obteniendo los datos: ', error);
     });
   }
+
+  sortOrder = -1;
+  sortOrderDetalle = 1;
+  currentColumn: string | undefined;
+  currentSortColumn: string = '';
+  sortAscending: boolean = true;
+
+  sortTableFecha(): void {
+    this.sortOrder = -this.sortOrder;
+    this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.sort((a: { fecha: string | number | Date; }, b: { fecha: string | number | Date; }) => {
+      let dateA = new Date(a.fecha);
+      let dateB = new Date(b.fecha);
+      return dateA > dateB ? this.sortOrder : dateA < dateB ? -this.sortOrder : 0;
+    });
+    if (this.currentSortColumn === 'fecha') {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      this.currentSortColumn = 'fecha';
+      this.sortAscending = true;
+    }
+  }
+
+  sortTableDetalle(): void {
+    this.sortOrderDetalle = -this.sortOrderDetalle;
+    this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.sort((a: { detalle: number; }, b: { detalle: number; }) => {
+      return a.detalle > b.detalle ? this.sortOrderDetalle : a.detalle < b.detalle ? -this.sortOrderDetalle : 0;
+    });
+    if (this.currentSortColumn === 'detalle') {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      this.currentSortColumn = 'detalle';
+      this.sortAscending = true;
+    }
+  }
+
+  /* sortTableNumero(column: string): void {
+    if (this.currentColumn === column) {
+      this.sortOrder = -this.sortOrder;
+    } else {
+      this.sortOrder = -1;
+      this.currentColumn = column;
+    }
+    this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.sort((a: { [x: string]: number; }, b: { [x: string]: number; }) => {
+      return a[column] > b[column] ? this.sortOrder : a[column] < b[column] ? -this.sortOrder : 0;
+    });
+  } */
+
+  sortTableNumero(column: string): void {
+    if (this.currentSortColumn === column) {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      this.currentSortColumn = column;
+      this.sortAscending = true;
+    }
+  
+    this.sortOrder = this.sortAscending ? 1 : -1;
+  
+    this.datosUsuarioActual.datosUsuario.montosUsuario.ctaCte.ctaCteTrans.sort((a: { [x: string]: number; }, b: { [x: string]: number; }) => {
+      return a[column] > b[column] ? this.sortOrder : a[column] < b[column] ? -this.sortOrder : 0;
+    });
+  }
+
+  isColumnRotated(column: string): boolean {
+    return this.currentSortColumn === column && this.sortAscending;
+  }
 }
