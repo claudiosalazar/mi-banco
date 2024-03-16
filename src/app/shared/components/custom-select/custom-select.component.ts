21064fa9b06a0049e8deaa9bba6680bc538e535f
Claudio Salazar <claudiosv@Mac-mini.local>
2024-03-16 04:27:54 -0300

    WIP Se inicia el componente con el value 0, pero no se imprimen los numeros de productos en option

--- src/app/shared/components/custom-select/custom-select.component.ts
+++ src/app/shared/components/custom-select/custom-select.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'custom-select',
   templateUrl: './custom-select.component.html'
 })
-export class CustomSelectComponent {
+export class CustomSelectComponent implements OnInit{
   @Input() control: FormControl | any;
   @Input() options: any[] = [];
   @Input() submitted: boolean | undefined;
@@ -13,6 +13,19 @@ export class CustomSelectComponent {
   @Input() labelSelect: string | undefined;
   selectedOption: any;
   isOpen: any;
+
+  ngOnInit() {
+    // Asegúrate de que las opciones estén definidas antes de intentar encontrar una opción
+    if (this.options) {
+      // Encuentra la opción con el valor '0'
+      let option = this.options.find(option => option.value === '0');
+      // Si la opción con el valor '0' existe, selecciona esta opción
+      if (option) {
+        this.selectedOption = option.label;
+        this.change.emit(option.value);
+      }
+    }
+  }
 
   selectOption(option: any) {
     this.selectedOption = option.label;
