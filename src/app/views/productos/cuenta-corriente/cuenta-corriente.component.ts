7510671f2eb29af3bcacb2054344588677324348
Claudio Salazar <claudiosv@MacBook-Pro-de-Claudio.local>
2024-03-17 02:28:04 -0300

    WIP El funcionamiento de los tab funciona correctamente desde header y en componente cuenta corriente pero se debe corregir la URL en el browser

--- src/app/views/productos/cuenta-corriente/cuenta-corriente.component.ts
+++ src/app/views/productos/cuenta-corriente/cuenta-corriente.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cuenta-corriente',
   templateUrl: './cuenta-corriente.component.html'
 })
-export class CuentaCorrienteComponent implements OnInit {
+export class CuentaCorrienteComponent implements OnInit, AfterViewInit  {
 
-  constructor() { }
+  activeTab = '';
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.activeTab = params['tab'] || 'movimientos';
+      this.updateActiveTab();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.updateActiveTab();
+  }
+
+  setActiveTab(tabId: string) {
+    this.activeTab = tabId;
+    this.updateActiveTab();
+  }
+
+  updateActiveTab() {
+    // Obtener todos los enlaces de las pestañas y eliminar la clase 'active'
+    var tabLinks = document.querySelectorAll('.nav-link');
+    tabLinks.forEach(link => link.classList.remove('active'));
+
+    // Agregar la clase 'active' al enlace de la pestaña deseada
+    var activeLink = document.querySelector(`.nav-link[href="#${this.activeTab}"]`);
+    if (activeLink) {
+      activeLink.classList.add('active');
+    }
+
+    // Obtener todas las pestañas y eliminar la clase 'active'
+    var tabs = document.querySelectorAll('.tab-pane');
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    // Agregar la clase 'active' a la pestaña deseada
+    var tab = document.querySelector(`#${this.activeTab}`);
+    if (tab) {
+      tab.classList.add('active');
+    }
   }
 
 }
