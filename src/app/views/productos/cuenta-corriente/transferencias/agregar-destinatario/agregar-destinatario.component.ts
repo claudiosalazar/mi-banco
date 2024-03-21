0ada400eab12b1746abd5e321304890e9217920b
Claudio Salazar <claudiosv@Mac-mini.local>
2024-03-21 17:46:50 -0300

    Se crearon offcanvas para crear y editar destinatarios sin logica. Se agregaron nuevos datos al json de destinatarios, se creo nuevo archivo scss para offcanvas y se agrego lupa de fondo para input de buscador

--- src/app/views/productos/cuenta-corriente/transferencias/agregar-destinatario/agregar-destinatario.component.ts
+++ src/app/views/productos/cuenta-corriente/transferencias/agregar-destinatario/agregar-destinatario.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-destinatario',
   templateUrl: './agregar-destinatario.component.html'
 })
-export class AgregarDestinatarioComponent {
+export class AgregarDestinatarioComponent implements OnInit{
+
+  crearDestinatarioForm: FormGroup = new FormGroup({});
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.crearDestinatarioForm = new FormGroup({
+      nombreDestinatario: new FormControl('', [Validators.required]),
+      apodoDestinatario: new FormControl(''),
+      rutDestinatario: new FormControl('', [Validators.required]),
+      bancoDestinatario: new FormControl('', [Validators.required]),
+      tipoCuentaDestinatario: new FormControl('', [Validators.required]),
+      numeroCuentaDestinatario: new FormControl('', [Validators.required]),
+      emailDestinatario: new FormControl('', [Validators.required]),
+      celularDestinatario: new FormControl(''),
+      telefonoDestinatario: new FormControl(''),
+    });
+  }
 
 }
--- src/app/views/productos/cuenta-corriente/transferencias/editar-destinatario/editar-destinatario.component.ts
+++ src/app/views/productos/cuenta-corriente/transferencias/editar-destinatario/editar-destinatario.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-destinatario',
   templateUrl: './editar-destinatario.component.html'
 })
-export class EditarDestinatarioComponent {
+export class EditarDestinatarioComponent implements OnInit{
+
+  editarDestinatarioForm: FormGroup = new FormGroup({});
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.editarDestinatarioForm = new FormGroup({
+      nombreDestinatario: new FormControl('', [Validators.required]),
+      apodoDestinatario: new FormControl('', [Validators.required]),
+      rutDestinatario: new FormControl('', [Validators.required]),
+      bancoDestinatario: new FormControl('', [Validators.required]),
+      tipoCuentaDestinatario: new FormControl('', [Validators.required]),
+      numeroCuentaDestinatario: new FormControl('', [Validators.required]),
+      emailDestinatario: new FormControl('', [Validators.required]),
+      celularDestinatario: new FormControl('', [Validators.required]),
+      telefonoDestinatario: new FormControl('', [Validators.required]),
+    });
+  }
 
 }
