ea2185678a08a020eaf19f344facd936ed2b5fd4
Claudio Salazar <contacto@claudiosalazar.cl>
2024-04-26 20:47:57 -0400

    Cuando el modal es mas alto que la pantalla se ajusta al top y genera scroll

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -127,10 +127,6 @@ export class HeaderComponent implements OnInit {
   abrirMenuMobile(): void {
     this.mostrarBackdropMenuMobile = !this.mostrarBackdropMenuMobile;
   }
-  
-  abrirSubmenu(): void {
-
-  }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(_event: any) {
