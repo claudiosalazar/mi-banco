90779a39b84daa5df845b41bf48c9028f49c236d
Claudio Salazar <contacto@claudiosalazar.cl>
2024-05-01 05:22:39 -0400

    WIP

--- src/app/mi-banco.component.ts
+++ src/app/mi-banco.component.ts
@@ -1,25 +1,19 @@
 // mi-banco.component.ts
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { MostrarBreadcrumbService } from './core/services/mostrar-breadcrumb.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './mi-banco.component.html'
 })
 export class MiBancoComponent implements OnInit {
+
   title = 'Mi Banco';
 
-  isResumenUsuarioPage = false;
-
   constructor(
-    private router: Router,
-    private mostrarBreadcrumbService: MostrarBreadcrumbService
+
   ) {}
 
   ngOnInit() {
-    this.mostrarBreadcrumbService.estadoActual.subscribe(estado => {
-      this.isResumenUsuarioPage = estado;
-    });
+    
   }
 }
