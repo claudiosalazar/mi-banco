9dfdff06fb0cf77cda86981352c0ddf5abb4ff9d
Claudio Salazar <contacto@claudiosalazar.cl>
2024-08-05 22:50:27 -0400

    Se muestra correctamente el inicio de cuenta corriente

--- src/app/components/private/views/productos/cuenta-corriente/cuenta-corriente.component.ts
+++ src/app/components/private/views/productos/cuenta-corriente/cuenta-corriente.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../../../../../services/productos.service';
 import { Productos } from '../../../../../models/productos.model';
 import { TransaccionesService } from '../../../../../services/transacciones.service';
-// import { Transacciones } from '../../../../../models/cuenta-corriente.model';
+import { CuentaCorriente } from '../../../../../models/cuenta-corriente.model';
+import { LineaCredito } from '../../../../../models/linea-credito.model';
+import { filter } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-cuenta-corriente',
@@ -10,13 +13,26 @@ import { TransaccionesService } from '../../../../../services/transacciones.serv
 })
 export class CuentaCorrienteComponent implements OnInit {
 
+  urlActual: any;
+
   productos: Productos[] = [];
-  //transacciones: Transacciones[] = [];
+  transaccionesCtaCte: CuentaCorriente[] = [];
+  transaccionesLineaCre: LineaCredito[] = [];
+  saldoUltimaTransaccionCtaCte: number | null = null;
+  fechaYhoraActual: Date = new Date();
+  saldo: any;
+  abono: any;
+  cargo: any;
+
+  activeTab: string = 'transferencias';
 
   constructor(
+    private route: Router,
     private productosService: ProductosService,
-    private transaccionesService: TransaccionesService
-  ) { }
+    private transaccionesService: TransaccionesService,
+  ) { 
+    this.urlActual = this.route.url;
+  }
 
   ngOnInit() {
     this.productosService.getProductos().subscribe((productos: Productos[]) => {
@@ -25,15 +41,29 @@ export class CuentaCorrienteComponent implements OnInit {
       }
     });
 
-    /*this.transaccionesService.getTransacciones().subscribe((transacciones: Transacciones[]) => {
-      if (transacciones) {
-        this.transacciones = transacciones;
+    this.transaccionesService.getTransCuentaCorriente().subscribe((transaccionesCtaCte: CuentaCorriente[]) => {
+      if (transaccionesCtaCte) {
+        this.transaccionesCtaCte = transaccionesCtaCte;
+        this.saldoUltimaTransaccionCtaCte = transaccionesCtaCte.length > 0 ? transaccionesCtaCte[transaccionesCtaCte.length - 1].saldo : null;
       }
-    });*/
-  }
+    });
 
-  /*getTransaccionesCtaCte() {
-    return this.transacciones.filter(transaccion => transaccion.id_producto === 0);
-  }*/
+    this.transaccionesService.getTransLineaCredito().subscribe((transaccionesLineaCre: LineaCredito[]) => {
+      if (transaccionesLineaCre) {
+        this.transaccionesLineaCre = transaccionesLineaCre;
+        const transaccionesOrdenadas = transaccionesLineaCre.sort((a, b) => b.id_trans_linea_cre - a.id_trans_linea_cre);
+        this.saldo = transaccionesOrdenadas[0]?.saldo;
+        this.cargo = transaccionesOrdenadas.find(trans => trans.cargo !== null)?.cargo;
+        this.abono = transaccionesOrdenadas.find(trans => trans.abono !== null)?.abono;
+      }
+    });
+
+    this.route.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+      ).subscribe((event: NavigationEnd) => {
+      this.urlActual = event.urlAfterRedirects;
+    });
+
+  }
 
 }
