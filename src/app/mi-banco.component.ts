d350fbdfcbbac0aa7ad8edeb3d335a90520f51c3
Claudio Salazar <contacto@claudiosalazar.cl>
2024-08-18 13:42:58 -0400

    Al actualizar el HTML este se posicion en el top de la pagina

--- src/app/mi-banco.component.ts
+++ src/app/mi-banco.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './mi-banco.component.html'
 })
-export class MiBancoComponent {
+export class MiBancoComponent implements OnInit, OnDestroy {
   title = 'Mi Banco';
+
+  ngOnInit() {
+    window.onload = function() {
+      window.scrollTo(0, 0);
+    };
+  }
+
+  ngOnDestroy() {
+    window.onload = null;
+  }
 }
