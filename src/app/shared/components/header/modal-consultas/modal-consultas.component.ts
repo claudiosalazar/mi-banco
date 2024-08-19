da2a13c95f5350597e2e15e45f0eafb137b07321
Claudio Salazar <contacto@claudiosalazar.cl>
2024-08-18 21:28:45 -0400

    Ajuste a modal consultas

--- src/app/shared/components/header/modal-consultas/modal-consultas.component.ts
+++ src/app/shared/components/header/modal-consultas/modal-consultas.component.ts
@@ -11,6 +11,18 @@ export class ModalConsultasComponent implements OnInit {
 
   @Output() consultaCancelada = new EventEmitter<void>();
 
+  listaConsultas = [
+    { value: '0', label: '-' },
+    { value: '1', label: 'Cuenta Corriente'},
+    { value: '2', label: 'Línea de Crédito'},
+    { value: '3', label: 'Tarjeta de Crédito'},
+    { value: '4', label: 'Crédito de Consumo'},
+    { value: '5', label: 'Transferencia de Fondos'},
+    { value: '6', label: 'Seguros' },
+    { value: '7', label: 'Otros productos o servicios'},
+    { value: '8', label: 'Reclamo'}
+  ];
+
   primer_nombre: any;
   segundo_nombre: any;
   apellido_paterno: any;
@@ -31,6 +43,7 @@ export class ModalConsultasComponent implements OnInit {
   consultaSeleccionada: any;
   nuevoValorConsulta: any;
   longitudTextoConsulta: number = 0;
+  textoConsulta: any;
 
   botonEnvioConsulta = false;
 
@@ -56,6 +69,76 @@ export class ModalConsultasComponent implements OnInit {
       consulta: new FormControl('0', [Validators.required]),
       textoConsulta: new FormControl('', [Validators.required]),
     });
+
+    this.validaConsulta();
+    this.validaListaConsulta();
+    this.validaFormulario();
+  }
+
+  validaListaConsulta(): any {
+    this.consultasForm.get('consulta')?.valueChanges.subscribe(value => {
+      if (value !== 0) {
+        // Se seleccionó un valor distinto a 0, el control de formulario es válido
+        this.consultasForm.get('consulta')?.setErrors(null);
+        this.consultaValida = true;
+      }
+    });
+  }
+
+  // Valida que se ingresaron mas de 50 caracteres
+  validaConsulta(): any {
+    this.consultasForm.get('textoConsulta')?.valueChanges.subscribe(value => {
+      this.longitudTextoConsulta = value.length;
+  
+      if (this.longitudTextoConsulta >= 50) {
+        this.consultasForm.get('textoConsulta')?.setErrors(null);
+      } else {
+        this.consultasForm.get('textoConsulta')?.setErrors({ 'minlength': true });
+      }
+    });
+  }
+    
+  // Valida region personal
+  validaFormulario(): any {
+    // Suscríbete a los cambios de estado de 'consulta'
+    this.consultasForm.get('consulta')?.statusChanges.subscribe(status => {
+      // Comprueba si ambos controles de formulario son válidos
+      if (this.consultasForm.get('consulta')?.valid && this.consultasForm.get('textoConsulta')?.valid) {
+        // Ambos controles de formulario son válidos, establece 'botonEnvioConsulta' en true
+        this.botonEnvioConsulta = true;
+      } else {
+        // Al menos uno de los controles de formulario es inválido, establece 'botonEnvioConsulta' en false
+        this.botonEnvioConsulta = false;
+      }
+    });
+  
+    // Suscríbete a los cambios de estado de 'textoConsulta'
+    this.consultasForm.get('textoConsulta')?.statusChanges.subscribe(status => {
+      // Comprueba si ambos controles de formulario son válidos
+      if (this.consultasForm.get('consulta')?.valid && this.consultasForm.get('textoConsulta')?.valid) {
+        // Ambos controles de formulario son válidos, establece 'botonEnvioConsulta' en true
+        this.botonEnvioConsulta = true;
+      } else {
+        // Al menos uno de los controles de formulario es inválido, establece 'botonEnvioConsulta' en false
+        this.botonEnvioConsulta = false;
+      }
+    });
+  }
+
+  activarSeleccionConsultas(): any {
+    this.consultasForm.get('consulta')?.valueChanges.subscribe(value => {
+      const consultaSeleccionada = this.listaConsultas.find(consulta => consulta.value === value);
+      this.consultaSeleccionada = consultaSeleccionada?.label;
+  
+      // Agrega un mensaje de consola para mostrar el valor seleccionado
+      console.log('Valor seleccionado:', value);
+
+      if (value === 0) {
+        // Si el valor de 'consulta' es 0, establece 'consultaValida' en false
+        this.consultasForm.get('consulta')?.setErrors(null);
+        this.consultaValidaReset = true;
+      }
+    });
   }
 
   enviaConsulta(): any {
@@ -69,7 +152,7 @@ export class ModalConsultasComponent implements OnInit {
     this.consultaCancelada.emit();
   }
 
-  cerrarModalFormiarioConsultas(): any {
+  cerrarModalFormularioConsultas(): any {
     this.consultaCancelada.emit();
   }
 
